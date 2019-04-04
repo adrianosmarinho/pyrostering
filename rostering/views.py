cbfe724600edd43b3d43ad8eba5b82325696d6f3
adrianosmarinho <adrianosmarinho@gmail.com>
2019-04-04 15:36:28 +1000

    added home page view and .gitignore file

--- rostering/views.py
+++ rostering/views.py
@@ -1,3 +1,20 @@
+from django.views import generic
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+#old version of home (using functions)
+# def home(request):
+#     return render(request, 'rostering/home.html')
+#     #old version
+#     #return HttpResponse("Hello, world. You're at the polls index.")
+
+
+class HomePageView(generic.TemplateView):
+
+    template_name = "rostering/home.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['latest_articles'] = Article.objects.all()[:5]
+    #     return context
