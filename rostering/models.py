6da6f09c9a04a49838ab03f0759911e890255571
adrianosmarinho <adrianosmarinho@gmail.com>
2019-04-21 17:33:09 +1000

    Implemented validations - a shift cannot be longer than 12 hours; an employee cannot work more than 5 days in a row

--- rostering/models.py
+++ rostering/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -25,10 +27,34 @@ class Employee(models.Model):
         if (len(self.first_name) != 0) and (len(self.last_name) != 0):
             super().save(*args, **kwargs)  # Call the "real" save() method.
 
-#TODO: Update here with the Shift model
+
 class Shift(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     date = models.DateField()
     start = models.TimeField()
     end = models.TimeField()
-    break_length = models.IntegerField() 
+    break_length = models.IntegerField()
+
+    def clean(self):
+        
+        # An employee cannot work more than 5 days in a row
+        # first, computes how many shifts an employee has
+        max_shifts_in_a_row = 5
+        all_shifts = self.employee.shift_set.all()
+        total_shifts = len(all_shifts)
+        offset = total_shifts - max_shifts_in_a_row
+        
+        # then, if it is more than 5, check for the errors
+        if ((offset) >= 0):
+            last_five_shifts = all_shifts[offset:]
+            # a flag for consecutive shifts, it will be false if any of the last max_shifts_in_a_row are not consecutive
+            consecutive = True
+            for i in range(max_shifts_in_a_row - 1):
+                if ((last_five_shifts[i+1].date - last_five_shifts[i].date).days > 1):
+                    consecutive = False
+                    break
+            
+            #at this point, we already have 5 consecutive shifts in the table, so cannot add another consecutive one
+            if (consecutive):
+                if ((self.date - last_five_shifts[max_shifts_in_a_row - 1].date).days) == 1:
+                    raise ValidationError(_('An employee cannot work more than five shifts in a row'))
